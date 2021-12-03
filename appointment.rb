f7306d6dd0126cd4075f9173b27e1e307e1bc6f3
ptt23 <lorenzo@etiner.com>
2021-12-03 20:53:30 -0300

    MonthlyAppointment

--- appointment.rb
+++ appointment.rb
@@ -1,2 +1,26 @@
 
 
+
+class Appointment
+    attr_accessor :location, :purpose, :hour, :min
+
+    def initialize(location,purpose,hour,min)
+        @location = location
+        @purpose = purpose
+        @hour = hour
+        @min = min
+    end
+end
+
+class MonthlyAppointment < Appointment
+    attr_accessor :day
+    def initialize(location,purpose,hour,min,day)
+        super(location,purpose,hour,min)
+        @day = day
+    end
+end
+
+pp MonthlyAppointment.new('NASA', 'Aliens', 8, 15, 23)
+
+
+
